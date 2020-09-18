f0f3a9dbb0642419b2bd48c6b9f03a72d4693ced
joserosalesdh <jm.rosales92@gmail.com>
2020-09-18 15:46:55 -0300

    configurando redux

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import '../../styles.css';
 import DateTimePicker from 'react-datetime-picker';
 import moment from 'moment';
+import Swal from 'sweetalert2';
 
 
 const customStyles = {
@@ -24,6 +25,23 @@ const CalendarModal = () => {
 
     const [dateStart, setDateStart] = useState(now.toDate());
     const [dateEnd, setDateEnd] = useState(nowPlus1.toDate());
+    const [titleValid, setTitleValid] = useState(true);
+
+    const [formValues, setFormValues] = useState({
+        title: 'Evento',
+        notes: '',
+        start: now.toDate(),
+        end: nowPlus1.toDate()
+    });
+
+    const { notes, title, start, end } = formValues;
+
+    const handleInputChange = ({ target }) => { //del evento solo me interesa el target
+        setFormValues({
+            ...formValues,
+            [target.name]: target.value // lo pongo entre [] porque quiero computar el valor de la propiedad
+        })
+    };
 
     const closeModal = () => {
 
@@ -31,10 +49,39 @@ const CalendarModal = () => {
 
     const handleStartDateChange = (e) => { //este evento va a ser la fecha
         setDateStart(e);
+        setFormValues({
+            ...formValues,
+            end: e
+        })
     };
 
     const handleEndDateChange = (e) => { //este evento va a ser la fecha
         setDateEnd(e);
+        setFormValues({
+            ...formValues,
+            start: e //el start es igual al evento que recibo 
+        })
+    };
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault(); //para evitar la propagación del formulario 
+
+        const momentStart = moment(start);
+        const momentEnd = moment(end);
+
+        if (momentStart.isSameOrAfter(momentEnd)) {
+            return Swal.fire('Error', 'La fecha fin debe de ser mayor a la fecha de inicio', 'error')
+        }
+
+        if (title.trim().length < 2) {
+            return setTitleValid(false)
+        }
+
+        //TODO realizar grabacion 
+
+        setTitleValid(true); //estas dos lineas es para cerrar la caja modal
+        closeModal();
+
     };
     return (
         <Modal
@@ -47,7 +94,10 @@ const CalendarModal = () => {
         >
             <h1> Nuevo evento </h1>
             <hr />
-            <form className="container">
+            <form
+                className="container"
+                onSubmit={handleSubmitForm}
+            >
 
                 <div className="form-group">
                     <label>Fecha y hora inicio</label>
@@ -73,10 +123,12 @@ const CalendarModal = () => {
                     <label>Titulo y notas</label>
                     <input
                         type="text"
-                        className="form-control"
+                        className={`form-control ${!titleValid && 'is-invalid'} `}
                         placeholder="Título del evento"
                         name="title"
                         autoComplete="off"
+                        value={title}
+                        onChange={handleInputChange}
                     />
                     <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
                 </div>
@@ -88,6 +140,8 @@ const CalendarModal = () => {
                         placeholder="Notas"
                         rows="5"
                         name="notes"
+                        value={notes}
+                        onChange={handleInputChange}
                     ></textarea>
                     <small id="emailHelp" className="form-text text-muted">Información adicional</small>
                 </div>
